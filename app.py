a881d94ba39ea59e56642a2dd878ab43f166519c
Florian ABGRALL <florian.abgralldwwb@gmail.com>
2021-11-25 14:41:51 +0100

    finished_app

--- app.py
+++ app.py
@@ -1,40 +1,86 @@
 import streamlit as st
 import pandas as pd
+import re
+import time
 
-st.write("""
-# Web Scraping App
-""")
+st.write(""" # Web Scraping App """)
 
 movie_db = pd.read_csv('./csvfiles/moviedb.csv')
 
+genre_list = movie_db['genre'].str.split(',').explode()
+actors_list = movie_db['actors'].str.split(',').explode()
+movie_db['duration'] = movie_db['duration'].fillna(0)
+movie_db['duration'] = movie_db['duration'].apply(lambda f: format(f, '.1f'))
+movie_db['duration'] = pd.to_datetime(movie_db['duration'], unit='m').dt.strftime('%H:%M')
+movie_db['years'] = movie_db['years'].fillna(0)
+movie_db['years'] = movie_db['years'].apply(lambda f: format(f, '.0f'))
+movie_db['note'] = movie_db['note'].apply(lambda f: format(f, '.1f'))
+movie_db['genre'] = movie_db['genre'].fillna('')
+movie_db['original_title'] = movie_db['original_title'].fillna('')
+movie_db['synopsis'] = movie_db['synopsis'].fillna('')
+movie_db['public'] = movie_db['public'].fillna('')
+
+country_list = movie_db['original_country'].str.split(',').explode().value_counts()
+
+
+st.write('### Top 250 greatest movies ')
+st.write(movie_db)
+
+my_cont = st.container() 
+
+
 def search_by_name():
-    name_indexes = movie_db.index[movie_db['actors'].str.contains(by_name_input)].tolist()
+    title_df = movie_db[movie_db['title'].str.contains(by_title_input, flags=re.IGNORECASE)]
     
-    return st.write(movie_db.iloc[name_indexes])
+    return st.write('### Your researched movie :', title_df)
 
 def search_by_actor():
-    pass
+    
+    actor_df = movie_db[movie_db['actors'].str.contains(by_actor_input, flags=re.IGNORECASE)]
+    
+    return st.write(f'### The movies with {by_actor_input} :', actor_df)
 
 def search_by_genre():
-    pass
+    genre_df = movie_db[movie_db['genre'].str.contains(by_genre_input, flags=re.IGNORECASE)]
+    
+    return st.write(f'### All the {by_genre_input} related movies :', genre_df)
 
-def search_by_length():
-    pass
+def search_by_duration():
+    duration_df = movie_db[movie_db['duration'].astype(str).str.contains(str(by_duration_input), flags=re.IGNORECASE)]
+    
+    return st.write(f'### Movies that length {by_duration_input} :', duration_df)
 
-def search_by_rating():
-    pass
+def search_by_rating():    
+    rating_df = movie_db[movie_db['note'].astype(str).str.contains(str(by_rating_input), flags=re.IGNORECASE)]
 
-by_name_input = st.text_input("Search by name", on_change=search_by_name)
+    return st.write(f'### Movies rated with {by_rating_input} :', rating_df)
 
-by_actor_input = st.text_input("Search by actor", on_change=search_by_actor)
+# by_title_input = st.sidebar.text_input("Search by title", on_change = search_by_name)
+by_title_input = st.sidebar.selectbox("Search by title", movie_db['title'].unique())
+by_actor_input = st.sidebar.selectbox("Search by actor", actors_list.unique())
+by_genre_input = st.sidebar.selectbox("Search by genre", genre_list.unique())
+by_duration_input = st.sidebar.selectbox("Search by duration", sorted(movie_db['duration'].unique(), reverse=True))
+by_rating_input = st.sidebar.selectbox("Search by rating", sorted(movie_db['note'].unique(), reverse=True))
+yes_top_ten = st.sidebar.button("You want to see a cool graph ???")
+start_load = st.sidebar.button("load load")
 
-by_genre_input = st.text_input("Search by genre", on_change=search_by_genre)
-
-by_length_input = st.text_input("Search by length", on_change=search_by_length)
-
-by_rating_input = st.text_input("Search by rating", on_change=search_by_rating)
-
-st.dataframe(movie_db)
-
-top_100_highest_rated = movie_db['note'].nlargest(n=10)
-st.bar_chart(top_100_highest_rated)
+with my_cont:
+    if by_title_input:
+        search_by_name()
+    if by_actor_input:
+        search_by_actor()
+    if by_genre_input:
+        search_by_genre()
+    if by_actor_input:
+        search_by_actor()
+    if by_duration_input:
+        search_by_duration()
+    if by_rating_input:
+        search_by_rating()
+    if yes_top_ten:
+        st.write(country_list)
+    if start_load:
+        my_bar = st.progress(0)
+        for percent_complete in range(100):
+            time.sleep(0.1)
+            my_bar.progress(percent_complete + 1) 
